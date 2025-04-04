b83f410fcd764178c80b3b3fbefcf48349c0911b
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-04-04 08:40:35 -0300

    Creating why me section

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <Counter />
     <Services />
     <Skills />
+    <Whyme />
   </div>
 </template>
 
@@ -15,4 +16,5 @@
   const Counter = defineAsyncComponent(() => import('@/components/layout/Counter.vue'));
   const Services = defineAsyncComponent(() => import('@/components/layout/Services.vue'));
   const Skills = defineAsyncComponent(() => import('@/components/layout/Skills.vue'));
+  const Whyme = defineAsyncComponent(() => import('@/components/layout/Whyme.vue'));
 </script>
---
+++ src/components/layout/Whyme.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="mt-32" id="whyme">
+    <SectionHeader title="Why Me?" />
+    <div class="grid grid-cols-1 lg:grid-cols-2 p-12 overflow-hidden">
+      <Education data-aos="fade-left" />
+      <Experience data-aos="fade-right" />
+    </div>
+  </section>
+</template>
+
+<script setup>
+  import SectionHeader from '../UI/SectionHeader.vue';
+  import Education from './Education.vue';
+  import Experience from './Experience.vue';
+</script>
