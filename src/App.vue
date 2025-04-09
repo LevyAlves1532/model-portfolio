3d6cc0653a8766fe492d4f0475f887eee637ba1a
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-04-09 08:28:08 -0300

    Creating Loading Spinner

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="bg-[#D4EBF8] dark:bg-[#080326] min-h-screen">
-    <Navbar />
-    <HeroSection />
-    <Counter />
-    <Services />
-    <Skills />
-    <Whyme />
-    <Projects />
-    <Contact />
-    <Footer />
-    <BackToTop />
+    <Suspense>
+      <template #default>
+        <div>
+          <Navbar />
+          <HeroSection />
+          <Counter />
+          <Services />
+          <Skills />
+          <Whyme />
+          <Projects />
+          <Contact />
+          <Footer />
+          <BackToTop />
+        </div>
+      </template>
+
+      <template #fallback>
+        <div class="flex justify-center items-center min-h-screen">
+          <LoadingSpinner />
+        </div>
+      </template>
+    </Suspense>
   </div>
 </template>
 
 <script setup>
   import { defineAsyncComponent } from 'vue';
+
   const Navbar = defineAsyncComponent(() => import('@/components/layout/Navbar.vue'));
   const HeroSection = defineAsyncComponent(() => import('@/components/layout/HeroSection.vue'));
   const Counter = defineAsyncComponent(() => import('@/components/layout/Counter.vue'));
@@ -25,4 +38,6 @@
   const Contact = defineAsyncComponent(() => import('@/components/layout/Contact.vue'));
   const Footer = defineAsyncComponent(() => import('@/components/layout/Footer.vue'));
   const BackToTop = defineAsyncComponent(() => import('@/components/layout/BackToTop.vue'));
+
+  import LoadingSpinner from './components/layout/LoadingSpinner.vue';
 </script>
